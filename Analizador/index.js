94d8f332136129083dac210a10d12e1ebb28342b
pato454 <patriciogomezjudo@gmail.com>
2026-05-18 15:58:48 -0300

    m

--- Analizador/index.js
+++ Analizador/index.js
@@ -15,17 +15,35 @@ function main() {
         console.error("Error: No se encontró 'input.txt'.");
         return;
     }
-
+    const erroresLexicos = [];
+    const erroresSintacticos = [];
 
     let inputStream = CharStreams.fromString(input);
     let lexer = new AnalizadorLexer(inputStream);
 
+    lexer.removeErrorListeners();
+    lexer.addErrorListener({
+        syntaxError: (recognizer, offendingSymbol, line, column, msg) => {
+            erroresLexicos.push({ linea: line, columna: column, mensaje: msg });
+        }
+    });
+
     const tokens = lexer.getAllTokens();
+
+    if (erroresLexicos.length > 0) {
+        console.log('Analisis fallido por errores lexicos: \n');
+        erroresLexicos.forEach(err => {
+            console.error(`-> [Línea ${err.linea}, Columna ${err.columna}]: Carácter no válido / ${err.mensaje}`);
+        });
+        console.log('Se aborta la ejecución.');
+        return; 
+    }
     
     if (tokens.length === 0) {
         console.error("Error, no se generaron tokens.");
         return;
     }
+
 
     console.log("TABLA DE LEXEMAS - TOKENS:\n");
     console.log("--------------------------------------------------");
@@ -43,13 +61,30 @@ function main() {
 
     inputStream = CharStreams.fromString(input);
     lexer = new AnalizadorLexer(inputStream);
+
+    lexer.removeErrorListeners();
+
     const tokenStream = new CommonTokenStream(lexer);
     const parser = new AnalizadorParser(tokenStream);
+    
+    parser.removeErrorListeners();
+        parser.addErrorListener({
+            syntaxError: (recognizer, offendingSymbol, line, column, msg) => {
+            erroresSintacticos.push({ linea: line, columna: column, mensaje: msg });
+        },
+        reportAmbiguity: () => {},
+        reportAttemptingFullContext: () => {},
+        reportContextSensitivity: () => {}
+    });
+    
     const tree = parser.programa(); 
 
     console.log("ANALISIS SINTAXTICO:")
-    if (parser.syntaxErrorsCount > 0) {
-        console.error("Se encontraron errores de sintaxis.");
+if (erroresSintacticos.length > 0) {
+        console.log("\nSe encontraron errores sintácticos:\n");
+        erroresSintacticos.forEach(err => {
+            console.error(`-> [Línea ${err.linea}, Columna ${err.columna}]: Estructura inválida - ${err.mensaje}`);
+        });
     } else {
         console.log("Análisis sintáctico exitoso.\n\n");
 
